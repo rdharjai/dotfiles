a1b35d4d7cb9413bd59d4858ae14c2729053d5ad
RD Harjai <rdharjai@gmail.com>
2020-05-16 12:34:46 -0500

    updates 05.16.2020

---
+++ linux-setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+
+# Update and Upgrade
+echo "Updating and Upgrading"
+sudo apt-get update && sudo apt-get upgrade -y
+
+
+# Install git
+echo "Installing Git, please congiure git later..."
+sudo apt-get install -y git
+
+
+# Install curl
+echo "Installing Curl"
+sudo apt-get install -y curl
+
+# Install tree
+echo "Installing Tree"
+sudo apt-get install -y tree
+
+
+# Python Packages
+sudo apt-get install python3-venv
+
+
+# Generate SSH Key
+SSH_FILE=~/.ssh/id_rsa.pub
+if test -f "$SSH_FILE"; then
+	echo "SSH key already esists. Skipping this step"
+else
+	echo "Generating SSH keys"
+	ssh-keygen -t rsa -b 4096
+fi
+
+
+# Install Sublime Text 3*
+echo "Installing Sublime Text"
+sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+
+# Install Google Chrome 
+echo "Installing Google Chrome"
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update 
+sudo apt-get install google-chrome-stable -y
+
+
+# Install VS Code
+echo "Installing VS Code"
+sudo snap install --classic code
+code --force --install-extension ms-python.python
+code --force --install-extension Equinusocio.vsc-material-theme 
+code --force --install-extension Equinusocio.vsc-material-theme-icons
+code --force --install-extension vscode-icons-team.vscode-icons
+code --force --install-extension PKief.material-icon-theme
+code --force --install-extension gruntfuggly.todo-tree 
+
+
+# LAST: Install ZSH, and Oh My Zsh
+echo "Installing ZSH"
+sudo apt-get install -y zsh
+sudo usermod -s /usr/bin/zsh $(whoami)
+chsh -s $(which zsh) $(whoami)
+
+# ZSH Fonts and Plugins
+sudo apt-get install -y powerline fonts-powerline
+
+
+# Oh My ZSH
+rm -rf ~/.oh-my-zsh
+rm ~/.zshrc*
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
+
---
+++ zshrc
@@ -0,0 +1,44 @@
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# Path to your oh-my-zsh installation.
+export ZSH="/Users/rdharjai/.oh-my-zsh"
+
+# Theme
+ZSH_THEME="rd-passion"
+
+# Plugins
+plugins=(
+	git 
+	zsh-syntax-highlighting 
+	zsh-autosuggestions
+)
+
+# source
+source $ZSH/oh-my-zsh.sh
+
+# alias - general
+alias mv='mv -i'
+alias sl='ls'
+alias ll='ls -alh'
+alias d='dirs -v | head -10'
+
+# alias - git
+alias gs='git status'
+alias ga='git add .'
+
+# alias - config files
+alias aconf='vim ~/.alacritty.yaml'
+alias aload='source ~/.alacritty.yaml'
+alias zconf='vim ~/.zshrc'
+alias zload='source ~/.zshrc'
+alias vconf='vim ~/.vimrc'
+alias vload='source ~/.vimrc'
+alias tconf='vim ~/.tmux.conf'
+alias tload='source ~/.tmux.conf'
+
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# Start tmux by default
+if [ "$TMUX" = "" ]; then tmux; fi
